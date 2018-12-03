10caf8fcaf21494f4d102617e41261d9b8b2a693
sammy9867 <samuelmenezes15@gmail.com>
2018-12-03 01:08:53 +0100

    New Separate LoginActivity

---
+++ app/src/main/java/com/example/samue/facultyblueprint/Login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.samue.facultyblueprint.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.samue.facultyblueprint.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    //Call Activity Login
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
