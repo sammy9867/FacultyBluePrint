aa797f66c8bd6df4b93aaf441de58e79adf226e0
michael <urmich.m@gmail.com>
2019-01-21 06:22:46 +0100

    List of the subjects on map-subject-details

--- app/src/main/java/com/example/samue/facultyblueprint/Maps/PopupDetails.java
+++ app/src/main/java/com/example/samue/facultyblueprint/Maps/PopupDetails.java
@@ -7,18 +7,24 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.samue.facultyblueprint.Classes.Teacher;
 import com.example.samue.facultyblueprint.Classes.Course;
 import com.example.samue.facultyblueprint.Login.User;
 import com.example.samue.facultyblueprint.R;
+import com.example.samue.facultyblueprint.SubjectsLists.SubjectDetailsActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PopupDetails extends Activity {
 
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,25 +50,44 @@ public class PopupDetails extends Activity {
         Intent i = getIntent();
         String roomNumber = (String) i.getSerializableExtra("roomNumber");
 
-        TextView tv = findViewById(R.id.room_number);
-        tv.setText(tv.getText() + "  " + roomNumber );
+        String roomText = "Room number:  " + roomNumber;
         int rm;
+        if(roomNumber.equalsIgnoreCase("105a")){ rm = 1051; }
+        else { rm = Integer.parseInt(roomNumber); }
 
-        if(roomNumber.equals("105a")){
-            rm = 1051;
-        }else {
-            rm = Integer.parseInt(roomNumber);
+        final ArrayList<Course> courses_on_list = new ArrayList<>();
+        List<String> subjects = new ArrayList<String>();
+
+        for (Course c : User.Courses){
+            if(c.room_number  == rm){
+//                ((TextView)findViewById(R.id.course_name)).setText(c.toString());
+//                ((TextView)findViewById(R.id.teacher_name)).setText("Teachers:\n"+c.listTeachers());
+                courses_on_list.add(c);
+                subjects.add(roomText + "\n" + c.toString() + "\n" + "Teachers:\n" + c.listTeachers());
+            }
         }
 
+        ListView listView = findViewById(R.id.roomsubjects_listview);
 
+        arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, subjects);
+        listView.setAdapter(arrayAdapter);
 
-       for (Course c : User.Courses){
-           if(c.room_number  == rm){
-               ((TextView)findViewById(R.id.course_name)).setText(c.toString());
-               ((TextView)findViewById(R.id.teacher_name)).setText("Teachers:\n"+c.listTeachers());
-               break;
-           }
-       }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Course selected_course = courses_on_list.get(position);
+
+                Intent intent = new Intent(getApplicationContext(), SubjectDetailsActivity.class);
+                intent.putExtra("SELECTED_COURSE_ID", selected_course.object_id);
+                startActivity(intent);
+
+            }
+        });
+
+        arrayAdapter.notifyDataSetChanged();
 
     }
 
